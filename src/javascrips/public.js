a5316f61fa5bf3d43b2ad5070480dc85fbd92323
liuzeaa <1098367891@qq.com>
2017-09-14 21:36:34 +0800

    201709142136

--- src/javascrips/public.js
+++ src/javascrips/public.js
@@ -5,3 +5,10 @@ if(index>-1){
     title = title.slice(index+1);
     $('#title').html(title);
 }
+
+
+var index = parent.layer.getFrameIndex(window.name); //先得到当前iframe层的索引
+$('#cancel').click(function(){
+    parent.layer.close(index); //再执行关闭
+})
+
